807173c95192b8c905ccf48c99611959ac604914
Mariojac8 <Mariojacobo8@yahoo.com>
2026-09-02 09:04:34 -0400

    Create Firstday.py for interest profile generator

---
+++ Firstday.py
@@ -0,0 +1,24 @@
+name="Mario"
+age=18
+ocupation="cyber student"
+
+intersts =[
+  "soccer",
+  "fishing",
+  "skiing",
+  "football",
+  "cards"
+  ]
+
+#welcome message
+print("welcome to the interest profile genorator")
+print("-----------------------------------------------------")
+
+print("name:",name)
+print("age:",age)
+print("ocupation",ocupation)
+
+#display interest
+print("\ninterest:")
+for interest in intersts:
+  print("-",interest)
